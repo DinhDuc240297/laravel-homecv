1c8bdd34a2208bc5c26ab2f97bbf82f7e707e0e0
Dinh Duc <nguyendinhduca5@gmail.com>
2018-04-21 14:52:56 +0700

    Add

---
+++ local/resources/views/test.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/styles.css')}}">
+</head>
+<body>
+	<h1>Nguyen dinh duc</h1>
+	<div class="hello">
+		<p>loroem	sc</p>
+	</div>
+	<header>Cao thu</header>
+	<footer>
+		@coppy right;
+	</footer>
+	
+</body>
+</html>
